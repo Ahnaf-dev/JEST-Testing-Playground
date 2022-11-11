94f7327f44cfac5d3bb2159672ed723c74016647
Ahnaf-dev <ahnafbari99@gmail.com>
2022-11-11 13:13:21 -0700

    handling hard cases for camelcase function

--- tests/camelCase.js
+++ tests/camelCase.js
@@ -1,12 +1,16 @@
 function camelCase(string) {
   let stringArray = string.split("-");
+
   let camelCased = stringArray
     .map((word, i) => {
-      if (i === 0) {
+      const firstWord = i === 0;
+
+      if (firstWord) {
         return word;
       }
 
-      return word[0].toUpperCase() + word.slice(1);
+      const capitalizedWord = word[0].toUpperCase() + word.slice(1);
+      return capitalizedWord;
     })
     .join("");
 
--- tests/camelcase.test.js
+++ tests/camelcase.test.js
@@ -1,6 +1,12 @@
 const camelCase = require("./camelCase");
 
 describe("camelCase", () => {
+  describe("when string such as border is given", () => {
+    it("should return border", () => {
+      expect(camelCase("border")).toBe("border");
+    });
+  });
+
   describe("when string such as text-transform is given", () => {
     it("should return textTransform", () => {
       expect(camelCase("text-transform")).toBe("textTransform");
